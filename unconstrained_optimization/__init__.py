e5a31aee86fad4af32acd93545da0470140a22cd
chriss1245 <christopher.manzano.vimos@gmail.com>
2021-05-18 09:44:07 +0200

    Added package init, added description of line search

---
+++ unconstrained_optimization/__init__.py
@@ -0,0 +1,1 @@
+from .lineSearch import lineSearch
--- unconstrained_optimization/lineSearch.py
+++ unconstrained_optimization/lineSearch.py
@@ -1,6 +1,16 @@
 import numpy as np
 
 def lineSearch(f, x_n, d_n, delta_alpha=0.01, convergence_threshold = 0.001):
+	"""
+	Given a function, a point x and a descent direction d It computes the alpha such that minimizes f(x + alpha*d)
+	Args:
+	-----------------------------------------------
+	f: function we want to maximize the minimization step
+	x_n: the point from which we want to start
+	d_n: the direction we want to go
+	delta_alpha: the increase in alpha
+	convergence_threshol: if the computed minimun does not change more than this threshold, the alpha is returned	
+	"""
 	current_min = np.Inf
 	alpha = 0
 	while True:
