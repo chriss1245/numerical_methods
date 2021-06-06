3e35b40bc3db0eef2dbd69f9f823f52b4b4c3378
chriss1245 <christopher.manzano.vimos@gmail.com>
2021-06-06 10:21:58 +0200

    made the formula more comprehensible, added method description

--- numerical_integration/simpsons_rule.py
+++ numerical_integration/simpsons_rule.py
@@ -1,6 +1,23 @@
 import numpy as np
-def simpsonIntegration(f, a, b,m):
+def simpsonIntegration(f, a, b, m) -> float :
+	"""
+	Takes a function and returns its definite integral betwen two points a and b with a stepsize m
+	using simpsons one third rule.
+	More info: https://en.wikipedia.org/wiki/Simpson%27s_rule
+	
+	Args:
+	-------------------------------
+	f: Funtion we want to integrate
+	a: lower bound
+	b: upper bound
+	m: stepsize
+
+	Returns:
+	-------------------------------
+	Integral of f evaluated 
+	"""
 	x = np.linspace(a,b,m)
-	return (1/3*m)*(b-a)*(f(x[0])+f(x[-1])+2*np.sum([f(x[k]) for k in range(2,len(x)/2, 2)])+4*np.sum([f(x[k]) for k in range(1, len(x)/2,2)]))
-
-
+	scaled_h = (b-a)/(3*m)
+	evenPart= 2*np.sum([f(x[k]) for k in range(2,len(x)/2, 2)])
+	oddPart = 4*np.sum([f(x[k]) for k in range(1, len(x)/2,2)])
+	return scaled_h*(f(x[0])+f(x[-1]) + evenPart + oddPart)
