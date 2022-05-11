fa7dc03d8ed979c4be93be6b020f0f566e406b04
Christoper Manzano <58918297+chriss1245@users.noreply.github.com>
2022-05-11 13:07:24 +0200

    Done by github copilot. Still need to test.

--- fourier/fastFourierTransform.py
+++ fourier/fastFourierTransform.py
@@ -1,1 +1,15 @@
-import numpy as np
+from numpy import pi, exp
+
+def fft(signal):
+    """
+    This function applies the fast fourier transform to a signal
+    """
+    N = len(signal)
+    if N == 1:
+        return signal
+    else:
+        even = fft(signal[::2])
+        odd = fft(signal[1::2])
+        T = [exp(-2j * pi * k / N) * odd[k] for k in range(N // 2)]
+        return [even[k] + T[k] for k in range(N // 2)] + [even[k] - T[k] for k in range(N // 2)]
+        
